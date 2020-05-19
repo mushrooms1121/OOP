75c7afcb22a00b329c9bcdff11eeed0df75d2916
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 23:31:01 +0800

    Update Point.h

--- 第二次期中小考/Point.h
+++ 第二次期中小考/Point.h
@@ -19,3 +19,37 @@ private:
 	int firstNumber, secondNumber;
 };
 #endif
+----------------------------------------------------------------------------------------------------------------------------------------
+#include "Point.h"
+
+Point::Point(int a, int b)
+{
+	firstNumber = a;
+	secondNumber = b;
+}
+int Point::getFirstNumber()
+{
+	return firstNumber;
+}
+int Point::getSecondNumber()
+{
+	return secondNumber;
+}
+Point Point::add(Point a)
+{
+	int f = firstNumber + a.getFirstNumber();
+	int s = secondNumber + a.getSecondNumber();
+	return Point(f, s);
+}
+Point Point::subtract(Point b)
+{
+	int f = firstNumber - b.getFirstNumber();
+	int s = secondNumber - b.getSecondNumber();
+	return Point(f, s);
+}
+string Point::toString()
+{
+	stringstream ss;
+	ss << "(" << firstNumber << "," << secondNumber << ")";
+	return ss.str();
+}
