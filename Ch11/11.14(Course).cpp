1092cb59d453041ccbc28eeb9643241f8558405e
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 20:42:37 +0800

    Update 11.14(Course).cpp

--- Ch11/11.14(Course).cpp
+++ Ch11/11.14(Course).cpp
@@ -50,4 +50,28 @@ int Course::getNumberOfStudents()const
 	return numberOfStudents;
 }
 ----------------------------------------------------------------------------------------------------------------------------------------
+#include "Course.h"
 
+int main()
+{
+	Course course1("Date Structures", 10); //µ¹student[capacity]°O¾ÐÅé10­Ó¦ì¸m
+	Course course2("Datebase System", 15);
+
+	course1.addStudent("Peter Jones");
+	course1.addStudent("Brian Smith");
+	course1.addStudent("Anne Kennedy");
+
+	course2.addStudent("Peter Jones");
+	course2.addStudent("Steve Smith");
+
+	cout << "Number of student in course1: " << course1.getNumberOfStudents() << endl;
+	string* student = course1.getStudent();
+	for (int i = 0; i < course1.getNumberOfStudents(); i++) cout << student[i] << ",";
+
+	cout << "\nNumber of student in course2: " << course2.getNumberOfStudents() << endl;
+	student = course2.getStudent();
+	for (int i = 0; i < course2.getNumberOfStudents(); i++) cout << student[i] << ",";
+
+	system("pause");
+	return 0;
+}
