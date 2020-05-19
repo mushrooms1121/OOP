073eda529de3fa9e6a094105d5527a495df25a37
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 22:48:22 +0800

    Add files via upload

---
+++ 第二次期中小考/Q2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+void reverse(const T list[], T newList[], int size)
+{
+	for (int i = 0, j = size - 1; i < size; i++, j--) newList[j] = list[i];
+}
+
+template<typename T>
+void printArray(const T list[], int size)
+{
+	for (int i = 0; i < size; i++) cout << list[i] << " ";
+}
+
+int main()
+{
+	const int SIZE = 6;
+	char list1[] = {'a','b','c','d','e','f'},newList[SIZE];
+	double list2[] = { 1.1,2.2,3.3,4.4,5.5,6.6 },newList2[SIZE]; //³£­n¦³newList[SIZE]
+
+	reverse(list1, newList, SIZE);
+	cout << "The original array: ";
+	printArray(list1, SIZE);
+	cout << endl;
+	cout << "The reversed array: ";
+	printArray(newList, SIZE);
+	cout << endl;
+
+	reverse(list2, newList2, SIZE);
+	cout << "The original array: ";
+	printArray(list2, SIZE);
+	cout << endl;
+	cout << "The reversed array: ";
+	printArray(newList2, SIZE);
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
