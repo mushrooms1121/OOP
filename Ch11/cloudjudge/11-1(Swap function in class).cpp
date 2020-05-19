546812e0c6e63fcd4b24e6b62d232ef7474091c1
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 21:38:24 +0800

    Update 11-1(Swap function in class).cpp

--- Ch11/cloudjudge/11-1(Swap function in class).cpp
+++ Ch11/cloudjudge/11-1(Swap function in class).cpp
@@ -1,3 +1,45 @@
+#include "Rectangle.h"
+
+Rectangle::Rectangle(double width, double height)
+{
+	this->width = width;
+	this->height = height;
+}
+void Rectangle::setWidth(double newWidth)
+{
+	width = newWidth;
+}
+double Rectangle::getWidth()
+{
+	return width;
+}
+void Rectangle::setHeight(double newHeight)
+{
+	height = newHeight;
+}
+double Rectangle::getHeight()
+{
+	return height;
+}
+double Rectangle::getArea()
+{
+	return width * height;
+}
+void Rectangle::swapByReference(Rectangle& r2) //pass by reference
+//對於參數而言 呼叫與被呼叫者 都使用相同的變數 如果被呼叫者改變了參數的數值 同樣的影響會影響到呼叫者的變數
+{
+	Rectangle temp = *this;
+	*this = r2;
+	r2 = temp;
+}
+void Rectangle::swapByPointer(Rectangle* r2) //pass by value
+//呼叫者與被呼叫者是兩個獨立的變數但使用相同的數值 如果被呼叫者更改了參數的數值 並不會影響到呼叫者本身
+{
+	Rectangle temp = *this;
+	*this = *r2;
+	*r2 = temp;
+}
+----------------------------------------------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <iomanip>
 #include "Rectangle.h"
