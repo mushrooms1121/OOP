e8653a88527da3f1cd44530fcd4860979272f88d
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 21:57:03 +0800

    Update Rectangle.cpp

--- Ch11/cloudjudge/Rectangle.cpp
+++ Ch11/cloudjudge/Rectangle.cpp
@@ -1,3 +1,21 @@
+#ifndef Rectangle_H
+#define Rectangle_H
+
+class Rectangle
+{
+public:
+	Rectangle(double width, double height);
+	void setWidth(double newWidth);
+	double getWidth();
+	void setHeight(double newHeight);
+	double getHeight();
+	double getArea();
+
+private:
+	double width, height;
+};
+#endif
+----------------------------------------------------------------------------------------------------------------------------------------
 #include "Rectangle.h"
 
 Rectangle::Rectangle(double width, double height)
@@ -25,3 +43,4 @@ double Rectangle::getArea()
 {
 	return width * height;
 }
+----------------------------------------------------------------------------------------------------------------------------------------
