2d65a15c115b2b4d202a447f50c24d70f136d603
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 20:37:50 +0800

    Add files via upload

---
+++ Ch11/11.9(CorrectReverse).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int* reverse(const int* list, int size)
+	{
+		int* result =new int[size]; //動態配置(例如:size是5 但要存10筆 那麼size就可以輕鬆存10筆)
+		//new會記憶體的每一格初始化 所以就不會有亂數產生
+		for (int i = 0, j = size - 1; i < size; i++, j--) result[j] = list[i];
+		return result;
+	}
+
+void printArray(const int* list, int size)
+{
+	for (int i = 0; i < size; i++) cout << list[i] << " ";
+}
+
+int main()
+{
+	int list[] = { 1,2,3,4,5,6 };
+	int* p = reverse(list, 6);
+	printArray(p,6);
+
+	system("pause");
+	return 0;
+}
