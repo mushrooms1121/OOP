c348754e84d48fa79031709d14274ac44fdbd623
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 20:48:14 +0800

    Update 11.19(CourseWithCustomCopyConstructor).cpp

--- Ch11/11.19(CourseWithCustomCopyConstructor).cpp
+++ Ch11/11.19(CourseWithCustomCopyConstructor).cpp
@@ -1,3 +1,28 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Course
+{
+public:
+	Course(const string& courseName, int capacity);
+	Course(const Course&);
+	string getCourseName()const;
+	void addStudent(const string& name);
+	void dropStudent(const string& name);
+	string* getStudent()const;
+	int getNumberOfStudents()const;
+
+private:
+	string courseName;
+	string* student;
+	int numberOfStudents, capacity;
+};
+
+#endif
+----------------------------------------------------------------------------------------------------------------------------------------
 #include "CourseWithCustomCopyConstructorcpp.h"
 
 Course::Course(const string& courseName, int capacity)
@@ -7,7 +32,7 @@ Course::Course(const string& courseName, int capacity)
 	this->capacity = capacity;
 	student = new string[capacity];
 }
-Course::Course(const Course& course) //�ƻs�غc��
+Course::Course(const Course& course) //½Æ»s«ØºcªÌ
 {
 	courseName = course.courseName;
 	numberOfStudents = course.numberOfStudents;
@@ -38,3 +63,4 @@ int Course::getNumberOfStudents()const
 {
 	return numberOfStudents;
 }
+----------------------------------------------------------------------------------------------------------------------------------------
