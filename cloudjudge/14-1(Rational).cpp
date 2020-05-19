394d1d3d7a3ef74fcaee78c727c8375d288375f9
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 23:52:31 +0800

    Update and rename Rational.h to 14-1(Rational).cpp

--- cloudjudge/14-1(Rational).cpp
+++ cloudjudge/14-1(Rational).cpp
@@ -226,3 +226,31 @@ void Rational::compareToVoid(const Rational& r2)
     else cout << numerator << "/" << denominator << ">" << r2.toString();
 }
 ----------------------------------------------------------------------------------------------------------------------------------------
+#include"Rational.h"
+
+int main() {
+
+	Rational f1(2, 3);
+	Rational f2(5, 7);
+	Rational f3(3, 5);
+
+	Rational s1(7, 2);
+	Rational s2(5, 6);
+	Rational s3(5, 7);
+
+	Rational first = f1 + f2 * f3;
+	Rational second = (s1 - s2) / s3;
+
+	cout << "first=" << first.toString() << endl;
+	cout << "second=" << second.toString() << endl;
+
+	first++;
+	cout << "++first=" << first.toString() << endl;
+	second--;
+	cout << "--second=" << second.toString() << endl;
+
+	first.compareToVoid(second);
+
+	system("pause");
+	return 0;
+}
