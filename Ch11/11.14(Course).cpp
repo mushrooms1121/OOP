330b87a827fdcb8c4315df1d7f57d633c4c39d32
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 20:42:01 +0800

    Update 11.14(Course).cpp

--- Ch11/11.14(Course).cpp
+++ Ch11/11.14(Course).cpp
@@ -1,3 +1,27 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Course
+{
+public:
+	Course(const string& courseName,int capacity);
+	string getCourseName()const;
+	void addStudent(const string& name);
+	void dropStudent()const;
+	string* getStudent()const;
+	int getNumberOfStudents()const;
+
+private:
+	string courseName;
+	string* student;
+	int numberOfStudents, capacity;
+};
+
+#endif
+----------------------------------------------------------------------------------------------------------------------------------------
 #include "Course.h"
 
 Course::Course(const string& courseName, int capacity)
@@ -25,3 +49,5 @@ int Course::getNumberOfStudents()const
 {
 	return numberOfStudents;
 }
+----------------------------------------------------------------------------------------------------------------------------------------
+
