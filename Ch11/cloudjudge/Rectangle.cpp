c2f53a4bc156d6bc9953a02927589529c77ba9c1
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 21:57:40 +0800

    Update Rectangle.cpp

--- Ch11/cloudjudge/Rectangle.cpp
+++ Ch11/cloudjudge/Rectangle.cpp
@@ -44,3 +44,39 @@ double Rectangle::getArea()
 	return width * height;
 }
 ----------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+#include <iomanip>
+#include "Rectangle.h"
+using namespace std;
+
+void swapByReference(Rectangle& r1, Rectangle& r2)
+{
+	Rectangle temp = r1;
+	r1 = r2;
+	r2 = temp;
+}
+void swapByPointer(Rectangle* r1, Rectangle* r2)
+{
+	Rectangle temp = *r1;
+	*r1 = *r2;
+	*r2 = temp;
+}
+
+int main()
+{
+	double w1, h1, w2, h2;
+	cin >> w1 >> h1 >> w2 >> h2;
+	Rectangle a1(w1, h1);
+	Rectangle a2(w2, h2);
+
+	cout << "SwapByReference:" << fixed << setprecision(2) << a1.getArea() << " " << a2.getArea() << " to ";
+	swapByReference(a1,a2);
+	cout << fixed << setprecision(2) << a1.getArea() << " " << a2.getArea() << endl;
+
+	cout << "SwapByPointer:" << fixed << setprecision(2) << a1.getArea() << " " << a2.getArea() << " to ";
+	swapByPointer(&a1,&a2);
+	cout << fixed << setprecision(2) << a1.getArea() << " " << a2.getArea() << endl;
+
+	system("pause");
+	return 0;
+}
