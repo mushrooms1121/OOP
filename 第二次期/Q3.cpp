596358688c265fbc6f639ea127b02168295f6485
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 23:32:00 +0800

    Update and rename 第二次期中小考/Point.h to 第二次期/Q3.cpp

--- 第二次期/Q3.cpp
+++ 第二次期/Q3.cpp
@@ -53,3 +53,22 @@ string Point::toString()
 	ss << "(" << firstNumber << "," << secondNumber << ")";
 	return ss.str();
 }
+----------------------------------------------------------------------------------------------------------------------------------------
+#include "Point.h"
+
+int main()
+{
+	int x[3], y[3];
+	for (int i = 0; i < 3; i++) cin >> x[i] >> y[i];
+
+	Point point1(x[0], y[0]);
+	Point point2(x[1], y[1]);
+	Point point3(x[2], y[2]);
+
+	cout << point1.toString() << "+" << point2.toString() << "-" << point3.toString() << "="
+		<< ((point1.add(point2)).subtract(point3)).toString() << endl;
+
+	system("pause");
+	return 0;
+	//¿é¤J1,0,-3,1,-4,3(¤@­Ó³r¸¹¤@¦¸enter)
+}
