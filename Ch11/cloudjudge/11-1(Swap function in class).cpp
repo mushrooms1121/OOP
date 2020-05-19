f2f4729e420005af21968de172e63d580f76a116
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-19 21:39:09 +0800

    Update 11-1(Swap function in class).cpp

--- Ch11/cloudjudge/11-1(Swap function in class).cpp
+++ Ch11/cloudjudge/11-1(Swap function in class).cpp
@@ -1,3 +1,23 @@
+#ifndef Rectangle_H
+#define Rectangle_H
+
+class Rectangle
+{
+public:
+	Rectangle(double width,double height);
+	void setWidth(double newWidth);
+	double getWidth();
+	void setHeight(double newHeight);
+	double getHeight();
+	double getArea();
+	void swapByReference(Rectangle& r2);
+	void swapByPointer(Rectangle* r2);
+
+private:
+	double width, height;
+};
+#endif
+----------------------------------------------------------------------------------------------------------------------------------------
 #include "Rectangle.h"
 
 Rectangle::Rectangle(double width, double height)
