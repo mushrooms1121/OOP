ee8bdaca9e7114ff1019bdf2be5f239c7d2b5dc0
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-20 10:12:06 +0800

    Update 11.18(Course).cpp

--- Ch11/11.18(Course).cpp
+++ Ch11/11.18(Course).cpp
@@ -57,10 +57,10 @@ int main()
 	Course course1("C++", 10);
 	Course course2(course1); //用了同一個記憶體
 
-	course1.addStudent("Peter Pan"); //因為上面有copy 所以原本student是Peter
-	course2.addStudent("Lisa Ma"); //後來又add Lisa覆蓋上原本的Peter
+	course1.addStudent("Peter Pan"); //因為上面有copy 所以原本student是Peter Pan
+	course2.addStudent("Lisa Ma"); //後來又add Lisa覆蓋上原本的Peter Pan
 
-	cout << "student in circle1: " << course1.getStudent()[0] << endl; //所以印出兩個student的值都是Lisa
+	cout << "student in circle1: " << course1.getStudent()[0] << endl; //所以印出兩個student的值都是Lisa Ma
 	cout << "student in circle2: " << course2.getStudent()[0] << endl;
 
 	system("pause");
