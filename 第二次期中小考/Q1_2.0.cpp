1dff9164b263c275de62b8b01b8b4c604b0a0b9a
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-05-20 10:59:48 +0800

    Add files via upload

---
+++ 第二次期中小考/Q1_2.0.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<vector<int>>m(4, vector<int>(3)); //vector是這樣宣告 記住!!!
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 3; j++) cin >> m[i][j];
+	}
+	int* min = min_element(&m[0][0], &m[4][3]);
+	cout << "min: " << *min << endl;
+
+	system("pause");
+	return 0;
+}
