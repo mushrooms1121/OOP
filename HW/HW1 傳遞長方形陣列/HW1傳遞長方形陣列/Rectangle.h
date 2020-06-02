f1bf769b9c9ed6d0c088432d363514e4db74ecc5
王韻淳 <58546596+mushrooms1121@users.noreply.github.com>
2020-06-02 23:23:13 +0800

    Add files via upload

---
+++ HW/HW1 傳遞長方形陣列/HW1傳遞長方形陣列/Rectangle.h
@@ -0,0 +1,21 @@
+#ifndef Rectangle_H
+#define Rectangle_H
+#include <iomanip>
+
+class Rectangle
+{
+public:
+	Rectangle();
+	Rectangle(double newWidth,double newHeight);
+	void setWidth(double newWidth);
+	void setHeight(double newHeight);
+	double getWidth()const;
+	double getHeight()const;
+	double getArea()const;
+	static int getNumOfRect();
+
+private:
+	double width, height;
+	static int numOfRect;
+};
+#endif
